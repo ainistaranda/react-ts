c72453cbda5878f275cc1665952642a158df57c8
Ainis Taranda <taranda.ainis@gmail.com>
2022-11-10 16:58:56 -0500

    used typescript to build react app, used interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
+import Greeting from './components/Greeting';
+import PhotoCard, { PhotoCardType } from './components/PhotoCard';
+import AboutMe, { Person } from './components/AboutMe';
+
+const me: Person = {
+  name: 'Todd Albert',
+  age: 47,
+  awesome: true
+}
+
+const card: PhotoCardType = {
+  photo: 'https://lh3.googleusercontent.com/ogw/AOh-ky34t5fd0w9ldRPKMVEIsV_xbcSYNHplZ9kVirThwQ=s32-c-mo',
+  user: 'Todd Albert',
+  mainPhoto: 'https://images.unsplash.com/photo-1665876702126-12c052834d42?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODExNjc0Ng&ixlib=rb-1.2.1&q=80&w=1080',
+  description: '',
+  likeCount: 99
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello TypeScript</h1>
+        <AboutMe person={me} />
+        <Counter />
+        <Greeting firstName='Ainis' lastName='Taranda' />
+        <Greeting firstName='Mike' lastName='Wazowski'/>
+        <PhotoCard card={card}/>
       </header>
     </div>
   );
